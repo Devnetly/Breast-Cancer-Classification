53b35c829f594fafb284fba7470c65b91ee57b5a
phues <yl.benyamina@esi-sba.dz>
2024-04-28 14:15:15 +0100

    feature extraction in tkinter

--- deployment/app.py
+++ deployment/app.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
+import torch
+from threading import Thread
+from feature_extraction import transform_wsis
 
 import sv_ttk
 
@@ -24,14 +27,34 @@ class FileOpener(tk.Tk):
         open_button = ttk.Button(self, text="Open", command=self.open_file)
         open_button.pack()
 
+        self.progress_var = tk.StringVar()
+        progress_label = ttk.Label(self, textvariable=self.progress_var)
+        progress_label.pack(pady=10)
+
     def browse_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[("WSI Files", "*.wsi")])
+        file_path = filedialog.askopenfilename(filetypes=[("WSI Files", "*.svs")])
         self.file_path_var.set(file_path)
 
     def open_file(self):
         file_path = self.file_path_var.get()
         if file_path:
-            print(f"Opening file: {file_path}")
+            thread = Thread(target=transform_wsis, kwargs={
+                'model': None,
+                'source_path': file_path,
+                'patch_size': 224,
+                'destination_folder': '/path/to/destination/folder',
+                'device': 'cuda' if torch.cuda.is_available() else 'cpu',
+                'prefetch_factor': 2,
+                'app': self
+            })
+            thread.start()
+
+    def update_progress(self, message):
+        def update():
+            self.progress_var.set(message)
+            self.update_idletasks()
+
+        self.after(0, update)
 
 if __name__ == "__main__":
     app = FileOpener()
--- deployment/feature_extraction.py
+++ deployment/feature_extraction.py
@@ -5,13 +5,14 @@ import argparse
 from torch import nn
 from torchvision.models.resnet import ResNet
 from torchvision.transforms import ToTensor,Resize,Compose
-from tqdm import tqdm
+from tqdm.tk import tqdm
 sys.path.append('../')
 from src.models import ResNet,ResNet18,ResNet34
 from src.utils import load_model_from_folder
 from src.datasets import WSIDataset
 from torch.utils.data import DataLoader
 import argparse
+import tkinter as tk
 
 def create_transforms(model : nn.Module,patch_size : int = 224):
 
@@ -31,7 +32,8 @@ def transform_wsis(
     patch_size : int,
     destination_folder : str,
     device : str,
-    prefetch_factor : int = 2
+    prefetch_factor : int = 2,
+    app : tk.Tk = None
 ) -> None:
     
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -39,7 +41,7 @@ def transform_wsis(
     model = None
 
     model = ResNet18(n_classes=3)
-    load_model_from_folder(model=model, weights_folder="/home/abdelnour/Documents/4eme_anne/S2/projet/models/resnet18",verbose=True)
+    load_model_from_folder(model=model, weights_folder="D:\\AIDS\\S2\\Project\\Breast Cancer Detection\\Breast-Cancer-Detection\\models\\resnet18",verbose=True)
     model.resnet.fc = nn.Identity()
     model = model.to(device)
 
@@ -53,21 +55,26 @@ def transform_wsis(
 
             basename = os.path.basename(source_path)
 
+            if app:  # Check if the app is provided
+                app.update_progress(f"Processing {source_path} : \n")
+
             print(f"Processing {source_path} : \n")
 
             dataset = WSIDataset(wsi_path=source_path,patch_size=patch_size,transform=transform)
-            loader = DataLoader(dataset=dataset,batch_size=32, num_workers=4, prefetch_factor=2)
+            loader = DataLoader(dataset=dataset,batch_size=32, num_workers=0, prefetch_factor=None)
             matrix = [[[] for _ in range(dataset.width)] for _ in range(dataset.height)]
 
-            for tiles, ws, hs in tqdm(loader):
+            for tiles, ws, hs in tqdm(loader, tk_parent=app):
 
                 tiles = tiles.to(device)
 
                 vectors = model(tiles).cpu()
 
-
                 for i, (w, h) in enumerate(zip(ws, hs)):
                     matrix[h][w] = vectors[i]
+
+                if app:  # Check if the app is provided
+                    app.update_progress(f"Processed {i+1}/{len(loader)} batches")
 
             matrix = [torch.stack(row, dim=0) for row in matrix]
             matrix = torch.stack(matrix, dim=0)
@@ -78,7 +85,8 @@ def transform_wsis(
             path_to_save,_ = os.path.splitext(path_to_save)
             path_to_save += f"{os.path.extsep}pth"
 
-            print(f"\n --- Whole slide image was saved to path : {path_to_save} -- \n")
+            if app: 
+                app.update_progress(f"\n --- Whole slide image was saved to path : {path_to_save} -- \n")  # Call the update_progress function
 
             pardir = os.path.dirname(path_to_save)
 
